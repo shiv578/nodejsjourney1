9ec078ed04209e5415ac9fc2bc9116fb96df75b2
shiv578 <swarajrkfl22@gmail.com>
2026-02-19 13:58:57 +0530

    added with any paramenrt

--- index4a.js
+++ index4a.js
@@ -15,7 +15,17 @@ const app = express();
 
 app.listen(8080, () => console.log("Server started"));
 
-app.get("/x/:a/y/:b/z/:c", (req, res) => {
-    const sum = +req.params.a + +req.params.b + +req.params.c;
-    res.send(sum.toString());
+app.get("/:x/:y/:b/:c", (req, res) => {
+  res.send("hello student")
 });
+
+
+// import express from "express";
+
+// const app = express();
+
+// app.listen(8080, () => console.log("Server started"));
+
+// app.use((req, res) => {
+//     res.send("hello student");
+// });
