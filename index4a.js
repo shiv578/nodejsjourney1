c0f63e13a9c592ca0c78a63c28e4cb63eefa725d
shiv578 <swarajrkfl22@gmail.com>
2026-02-19 13:46:07 +0530

    updated

--- index4a.js
+++ index4a.js
@@ -1,10 +1,21 @@
+// import express from "express";
+
+// const app = express();
+
+// app.listen(8080, () => console.log("Server started"));
+
+// app.get("/:a/:b", (req, res) => {
+//     const sum = +req.params.a + +req.params.b;
+//     res.send(sum.toString());
+// });
+
 import express from "express";
 
 const app = express();
 
 app.listen(8080, () => console.log("Server started"));
 
-app.get("/:a/:b", (req, res) => {
-    const sum = +req.params.a + +req.params.b;
+app.get("/x/:a/y/:b/z/:c", (req, res) => {
+    const sum = +req.params.a + +req.params.b + +req.params.c;
     res.send(sum.toString());
 });
