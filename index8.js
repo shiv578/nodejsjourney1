5feff47a6d5df43a8b6d79dd1f85e941df9a7986
shiv578 <swarajrkfl22@gmail.com>
2026-02-20 15:46:30 +0530

    pushed index 9

--- index8.js
+++ index8.js
@@ -1,22 +1,44 @@
-import express from 'express';
-const app=express();
-app.listen(8080);
-const users=[
-    {id:1,name:"john",email:"john@gmail.com",role:"student"},
-    {id:2,name:"john",email:"john@gmail.com",role:"student"},
-    {id:3,name:"john",email:"john@gmail.com",role:"student"},
 
-]
-// app.get("/",( req, res )=>{
-//     res.json(users);  
+import express, { request, response } from "express";
+const app = express();
+app.listen(8080, () => {
+  console.log("Server Started....");
+});
 
-// })
-app.get("/:id",(req,res)=>{
-    const id = Number(req.params.id);
-    const user = users.find(u => u.id === id);
+// const greet = (req,res,next) => {
+//   console.log("Hello World");
+//   next()
+  
+// };
+// app.use(greet)
 
-    if(!user){
-        return res.status(404).json({message:"User not found"});
-    }
-    res.json(user);
+app.use(express.json());
+
+let users = [
+  { id: 1, name: "avi", email: "avi@mail.com", role: "Student" },
+  { id: 2, name: "satyam", email: "satyam@mail.com", role: "admin" },
+  { id: 3, name: "vivaan", email: "vivaan@mail.com", role: "Student" },
+];
+
+app.get("/", (request, response) => {
+  response.json(users);
+});
+
+app.post("/", (request, response) => {
+  //   console.log(request.body);
+  const user = request.body
+  users.push(user);
+  response.json(users);
+});
+
+app.delete("/:id", (request, response) => {
+    users = users.filter((user) => user.id !== Number(req.params.id))
+    res.json(users)
+});
+
+app.get("/:id", (request, response) => {
+  const user = users.find((user) => user.id === Number(request.params.id));
+  if (user) {
+    response.json(user);
+  }
 });
